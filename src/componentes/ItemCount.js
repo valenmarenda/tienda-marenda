4dddab3684dad004588101375be448fcd2143820
Valentina <valentinamarenda1@gmail.com>
2021-05-17 21:28:52 -0300

    cuarto commit

---
+++ src/componentes/ItemCount.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import './ItemCount.css'
+
+
+
+const ItemCount = ({ stock, initial}) => {
+ const [stockRequired, setStockRequired] = useState(initial);
+ 
+ const onAdd = (value) => {
+     if ((stock >= stockRequired + value ) && ((stockRequired + value) >=0) ) {
+     setStockRequired(stockRequired + value)
+    } else{
+
+    }
+ }
+
+ 
+ 
+ return (
+     <div className="containerContador">
+         <h1>Contador de productos</h1>
+         <div className="containerItemCount">
+            <button className="itemCount" onClick={() => onAdd(+1)}><i class="fas fa-plus"></i></button>
+             <h4> {stockRequired} </h4>
+             <button className="itemCount" onClick={() => onAdd(-1)}><i class="fas fa-minus"></i></button>
+         </div>   
+     </div>
+ )
+}
+
+export default ItemCount; 
--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,8 +1,13 @@
 import React, { } from 'react'
+import ItemCount from './ItemCount'
+import './ItemListContainer.css';
 
 function ItemListContainer(){
 return(
+    <div className="ItemListContainer">
     <h1>Aqui irá el catálogo <span>😻</span></h1>
+    <ItemCount stock={5} initial={1}/>
+    </div>
 )
 }
 
